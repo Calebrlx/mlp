9438c9710022c2fafe13b322347551a71278127c
Caleb Forestal | <133164966+Calebrlx@users.noreply.github.com>
2024-03-17 14:54:02 -0800

    Create set-up.sh

---
+++ set-up.sh
@@ -0,0 +1,10 @@
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew update
+
+
+brew install python@3.10
+
+brew install pyenv
+pyenv install 3.10.0
